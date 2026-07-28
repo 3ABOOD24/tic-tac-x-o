1c5169bed9581f45f327f7707572bb22b9d19f9f
3ABOOD24 <ahorses2020@gmail.com>
2026-07-28 23:24:11 +0300

    update prototype

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 const message = document.getElementById("message")
-const score1 = document.querySelector(".score1")
-const score2 = document.querySelector(".score2")
+const score1 = document.querySelector("#score1")
+const score2 = document.querySelector("#score2")
 const boxes = document.querySelectorAll(".box") 
 const btn1 = document.querySelector(".button1")
 const btn2 = document.querySelector(".button2")
 const btn3 = document.querySelector(".button3")
 const player1 = { name:"player1", Symbol:"X", score: 0}
 const player2 = { name:"player2", Symbol:"O", score: 0}
-let playing = player1
+let playing = "player1"
 const board = ["", "", "", "", "", "", "", "", ""]
-const box1 = document.querySelector("#1")
-const box2 = document.querySelector("#2")
-const box3 = document.querySelector("#3")
-const box4 = document.querySelector("#4")
-const box5 = document.querySelector("#5")
-const box6 = document.querySelector("#6")
-const box7 = document.querySelector("#7")
-const box8 = document.querySelector("#8")
-const box9 = document.querySelector("#9")
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -28,16 +19,63 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-function newGame() {
-    playing = player1
+function restart(){
+    playing = "player1"
+    score1.innerText = "0"
+    score2.innerText = "0"
     board.fill("")
+    boxes.forEach((box) => {
+        box.innerText = ""
+    })
 }
-newGame()
-btn1.addEventListener("click", () => {
-    message.classList.add("nono");
+function round(){
+    playing = "player1"
+    board.fill("")
+    boxes.forEach((box) => {
+        box.innerText = ""
+    })
+    message.innerText = ''
+}
+function put(lol){
+    const cell = lol.target
+    let indexx = Number(cell.dataset.cellIndex)
+    if(board[indexx] !== ""){
+        return
+    }
+    if(playing === "player1"){
+        board[indexx] = player1.Symbol
+        cell.classList.remove("O")
+        cell.classList.add("X")
+        cell.innerText = player1.Symbol
+        playing = "player2"
+        console.log(board)
+    }
+    else{
+        board[indexx] = player2.Symbol
+        cell.classList.remove("X")
+        cell.classList.add("O")
+        cell.innerText = player2.Symbol
+        playing = "player1"
+        console.log(board)
+    }
+    winningConditions.forEach((condition) => {
+        const [a,b,c] = condition
+        if(board[a] == board[b] && board[b] == board[c] && board[a] == board[c] && board[a] != ""){
+            if(playing == "player1"){
+                player2.score++
+                score2.innerText = `${player2.score}`
+                message.innerHTML = `player 2 won 😝`
+            }
+            if(playing == "player2"){
+                player1.score++
+                score1.innerText = `${player1.score}`
+                message.innerHTML = `player 1 won 😝`
+            }
+        }
+    })
+}
+boxes.forEach((box, index) => {
+    box.addEventListener("click", put)
 })
-btn2.addEventListener("click", () => {
-    message.classList.remove("nono");
-    newGame()    
-})
-box
+btn3.addEventListener("click", restart)
+btn2.addEventListener("click", round)

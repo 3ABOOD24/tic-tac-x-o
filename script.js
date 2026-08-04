803e36281ba7155c19323040323130d3fd22cbec
3ABOOD24 <ahorses2020@gmail.com>
2026-08-04 22:35:21 +0300

    prototype 2.1

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// readonly
+const card = document.querySelector(".card")
+const container = document.querySelector(".container")
 const message = document.getElementById("message")
 const score1 = document.querySelector("#score1")
 const score2 = document.querySelector("#score2")
 const boxes = document.querySelectorAll(".box")
 const btn2 = document.querySelector(".button2")
 const btn3 = document.querySelector(".button3")
-const player1 = { name:"player1", Symbol:"X", score: 0}
-const player2 = { name:"player2", Symbol:"O", score: 0}
+const player1 = { name: "player1", Symbol: "X", score: 0 }
+const player2 = { name: "player2", Symbol: "O", score: 0 }
 let playing = "player1"
 const board = ["", "", "", "", "", "", "", "", ""]
 const winningConditions = [
@@ -18,7 +21,7 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-function restart(){
+function restart() {
     playing = "player1"
     score1.innerText = "0"
     score2.innerText = "0"
@@ -29,21 +32,23 @@ function restart(){
         box.innerText = ""
     })
     message.innerText = ''
+    card.style.display = 'flex'
+    container.style.display = 'none'
 }
-function round(){
+function round() {
     board.fill("")
     boxes.forEach((box) => {
         box.innerText = ""
     })
     message.innerText = ''
 }
-function put(lol){
+function put(lol) {
     const cell = lol.target
     let indexx = Number(cell.dataset.cellIndex)
-    if(board[indexx] !== ""){
+    if (board[indexx] !== "") {
         return
     }
-    if(playing === "player1"){
+    if (playing === "player1") {
         board[indexx] = player1.Symbol
         cell.classList.remove("O")
         cell.classList.add("X")
@@ -51,7 +56,7 @@ function put(lol){
         playing = "player2"
         console.log(board)
     }
-    else{
+    else {
         board[indexx] = player2.Symbol
         cell.classList.remove("X")
         cell.classList.add("O")
@@ -60,14 +65,14 @@ function put(lol){
         console.log(board)
     }
     winningConditions.forEach((condition) => {
-        const [a,b,c] = condition
-        if(board[a] == board[b] && board[b] == board[c] && board[a] == board[c] && board[a] != ""){
-            if(playing == "player1"){
+        const [a, b, c] = condition
+        if (board[a] == board[b] && board[b] == board[c] && board[a] == board[c] && board[a] != "") {
+            if (playing == "player1") {
                 player2.score++
                 score2.innerText = `${player2.score}`
                 message.innerHTML = `player 2 won 😝`
             }
-            if(playing == "player2"){
+            if (playing == "player2") {
                 player1.score++
                 score1.innerText = `${player1.score}`
                 message.innerHTML = `player 1 won 😝`
